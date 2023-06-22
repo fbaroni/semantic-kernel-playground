dd7ca532610824fa66773e727dd53fb9f171d470
Federico <>
2023-06-22 17:58:53 +0200

    configured light theme. Added vector search

--- app_chat.py
+++ app_chat.py
@@ -2,9 +2,28 @@ from dotenv import load_dotenv
 load_dotenv()
 import streamlit as st
 import os
-from azure.search.documents import SearchClient
-from azure.core.credentials import AzureKeyCredential
 import openai
+from tenacity import retry, wait_random_exponential, stop_after_attempt  
+from azure.core.credentials import AzureKeyCredential  
+from azure.search.documents import SearchClient  
+from azure.search.documents.indexes import SearchIndexClient  
+from azure.search.documents.models import Vector  
+from azure.search.documents.indexes.models import (  
+    SearchIndex,  
+    SearchField,  
+    SearchFieldDataType,  
+    SimpleField,  
+    SearchableField,  
+    SearchIndex,  
+    SemanticConfiguration,  
+    PrioritizedFields,  
+    SemanticField,  
+    SearchField,  
+    SemanticSettings,  
+    VectorSearch,  
+    VectorSearchAlgorithmConfiguration,  
+)  
+
 
 # logger = logging.getLogger('azure.core.pipeline.policies.http_logging_policy').setLevel(logging.WARNING)
 # Set up the search client
@@ -18,12 +37,19 @@ openai.api_type = "azure"
 openai.api_version = "2023-05-15" 
 openai.api_base = os.getenv("AZURE_OPENAI_ENDPOINT") 
 
-# Define the query function
-def run_query(query):
-    # TODO vector search
-    results = client.search(search_text=query, top=3)
-    return results.get_results()
+def generate_embeddings(text):
+    response = openai.Embedding.create(
+        input=text, engine="text-embedding-ada-002")
+    embeddings = response['data'][0]['embedding']
+    return embeddings
 
+def run_query(query):
+    
+    return client.search(  
+        search_text="",  
+        vector=Vector(value=generate_embeddings(query), k=3, fields="contentVector"),  
+        select=["title", "content", "category"] 
+    )  
 def get_highlighted_text(query, content):
 
     messages = [
@@ -71,7 +97,7 @@ def app():
     st.title("Search query")
     query = st.text_input("Enter your query here: ")
     if st.button("Search"):
-        results = client.search(search_text=query, top=1)
+        results = client.search(search_text=query, top=3)
         for result in results:
             text = get_highlighted_text(query, result['content'])
             st.write("<a href='#'>Go to the document</a>", unsafe_allow_html=True)
